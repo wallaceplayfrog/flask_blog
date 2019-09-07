a14c620c71c86d0267b956675b0c74f543caf6a2
wallacepalyfrog <2425779559@qq.com>
2019-09-07 14:35:38 +0800

    4a

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment 
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+# 定义表单类
+# 包含一个文本字段和一个提交按钮
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.secret_key = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data 
+        form.name.data = ''
+    return render_template('index.html', form = form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
