d5f5ab3e1476672a87d379244adf9fbafd9e9f5e
wallacepalyfrog <2425779559@qq.com>
2019-09-07 14:48:28 +0800

    4b

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment 
+from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -19,12 +21,11 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data 
-        form.name.data = ''
-    return render_template('index.html', form = form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
