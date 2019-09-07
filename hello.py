5659aeeced89a867fdd30810b17a96e0a953eada
wallacepalyfrog <2425779559@qq.com>
2019-09-07 14:58:22 +0800

    4c

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment 
 from datetime import datetime
@@ -23,6 +23,10 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            # 闪现消息
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form = form, name=session.get('name'))
