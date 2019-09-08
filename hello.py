2a89d7feb550785c8ce110a57851ea6743159967
wallacepalyfrog <2425779559@qq.com>
2019-09-08 12:58:41 +0800

    5b

--- hello.py
+++ hello.py
@@ -54,13 +54,19 @@ class User(db.Model):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            # 闪现消息
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
