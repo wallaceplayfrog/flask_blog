d82438592ffe8bf49e2d65934530710481d894ae
wallacepalyfrog <2425779559@qq.com>
2019-09-08 19:34:32 +0800

    用户登录模块

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,26 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
 from . import auth
+from flask_login import login_user, login_required, logout_user
+from ..models import User
+from .forms import LoginForm 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('错误的用户名或密码')
+    return render_template('auth/login.html', form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('登出完成')
+    return redirect(url_for('main.index'))
