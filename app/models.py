12dc1cf293a1fb1a558c9308ec43de9b8eb923ab
wallacepalyfrog <2425779559@qq.com>
2019-09-08 16:02:19 +0800

    在User模型中加入密码散列

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # 定义Role和User模型
 class Role(db.Model):
@@ -18,6 +19,19 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     # 建立外键
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    # 在User模型中加入密码散列
+    password_hash = db.Column(db.String(128))
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite://' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite://'
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
