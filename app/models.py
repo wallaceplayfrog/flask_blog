672ed625bd12f86f1f52f7366eb4657d47bb7170
wallacepalyfrog <2425779559@qq.com>
2019-09-13 16:25:18 +0800

    生成随机信息

--- app/models.py
+++ app/models.py
@@ -111,7 +111,7 @@ class User(UserMixin, db.Model):
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
-            if self.email == current_app.config['FLASK_ADMIN']:
+            if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
