37373c39809d4bdc9a6799628bb8896d3e530ae0
wallacepalyfrog <2425779559@qq.com>
2019-09-18 17:28:39 +0800

    restful support

--- app/__init__.py
+++ app/__init__.py
@@ -35,4 +35,8 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    # 注册api蓝本
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+
     return app
